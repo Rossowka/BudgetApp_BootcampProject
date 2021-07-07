0d17a3914c495c6e7daf56159a4291199b347aa6
Sara Rossow <rossowka@icloud.com>
2021-07-07 19:42:05 +0200

    added balance statement

--- index.js
+++ index.js
@@ -17,3 +17,12 @@ const expenseAmount = document.getElementById('expense-amount');
 let balance = 0, income = 0, expense = 0;
 let ENTRY_LIST;
 
+function updateBalanceStatement(balance) {
+    if (balance < 0) {
+        balanceStatement.innerHTML = `Ooops! You are <span class="deep-orange-text text-lighten-2">${Math.abs(balance)}</span> PLN under the budget..`
+    } else if (balance > 0){
+        balanceStatement.innerHTML = `You can still spend <span class="teal-text text-lighten-2">${balance}</span> PLN`
+    } else if (balance == 0){
+        balanceStatement.innerHTML = `You have achieved a perfect balance! 🥳`
+    };
+}
