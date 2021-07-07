665b00551a42a32b53db6fed695ffb2761870a03
Sara Rossow <rossowka@icloud.com>
2021-07-07 19:28:13 +0200

    linked DOM elements with variables

---
+++ index.js
@@ -0,0 +1,14 @@
+// SELECT ELEMENTS & BUTTONS
+const totalIncomeEl = document.getElementById('total-income');
+const totalExpenseEl = document.getElementById('total-expense');
+const balanceStatement = document.getElementById('budget-summary');
+const incomeTable = document.getElementById('income-table');
+const expenseTable = document.getElementById('expense-table');
+const addIncomeBtn = document.getElementById('add-income');
+const addExpenseBtn = document.getElementById('add-expense');
+
+//INPUT
+const incomeName = document.getElementById('income-name');
+const incomeAmount = document.getElementById('income-amount');
+const expenseName = document.getElementById('expense-name');
+const expenseAmount = document.getElementById('expense-amount');
